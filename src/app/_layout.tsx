c7f5ce1481794cc20cde14de314be10b471fe87d
MCXLVI <84153848+MCXLVI@users.noreply.github.com>
2026-03-15 21:12:12 -0400

    idk if it works

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="my-day">
+        <NativeTabs.Trigger.Label>My Day</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="clock" md="schedule" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
